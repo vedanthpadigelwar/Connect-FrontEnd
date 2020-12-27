ee9b700ae7b6731b43f9bd95865b7f0ee75815f5
vedanth padigelwar <vedanthpadigelwar@gmail.com>
2020-12-27 17:28:20 +0530

    icons_error_fixed

--- Connect/screens/DrawerContent.js
+++ Connect/screens/DrawerContent.js
@@ -93,7 +93,7 @@ export function DrawerContent(props){
                         <DrawerItem 
                             icon={({color, size}) => (
                                 <Icon 
-                                name="settings-outline" 
+                                name="settings-helper" 
                                 color={color}
                                 size={size}
                                 />
--- Connect/screens/MainTabScreen.js
+++ Connect/screens/MainTabScreen.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './HomeScreen';
