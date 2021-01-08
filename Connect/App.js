973044a6677dc3a3185cbab413c79a70146b07dd
vedanth padigelwar <vedanthpadigelwar@gmail.com>
2021-01-08 18:00:16 +0530

    update App.js
    checking working

--- Connect/App.js
+++ Connect/App.js
@@ -16,15 +16,15 @@ const Drawer = createDrawerNavigator();
 const App = () =>{
   return (
     <NavigationContainer>
-      <RootStackScreen/>
-      {/*
+      {/* <RootStackScreen/> */}
+      
       <Drawer.Navigator drawerContent={props => <DrawerContent {...props}/>}>
           <Drawer.Screen name="HomeDrawer" component={MainTabScreen} />
           <Drawer.Screen name="SupportScreen" component={SupportScreen} />
           <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
           <Drawer.Screen name="BookmarkScreen" component={BookmarkScreen} />
 
-      </Drawer.Navigator>*/}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
